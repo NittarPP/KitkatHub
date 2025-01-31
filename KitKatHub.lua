ec42825d31e1122205f5c1e99fe1e0c21e475c79
Kokie <167613469+NittarPP@users.noreply.github.com>
2025-01-31 22:26:33 +0700

    Update KitKatHub.lua

--- KitKatHub.lua
+++ KitKatHub.lua
@@ -3,6 +3,20 @@ Look My LICENSE
 https://raw.githubusercontent.com/NittarPP/KitkatHub/refs/heads/main/LICENSE
 ]]
 
+local response = request({
+    Url = "https://httpbin.org/user-agent",
+    Method = "GET",
+})
+assert(type(response) == "table", "Response must be a table")
+assert(response.StatusCode == 200, "Did not return a 200 status code")
+local data = game:GetService("HttpService"):JSONDecode(response.Body)
+assert(type(data) == "table" and type(data["user-agent"]) == "string", "Did not return a table with a user-agent key")
+
+if not data["user-agent"]:lower():match("flexer") or not data["user-agent"]:lower():match("xeno") then
+    game.Players.LocalPlayer:Kick("KitKat Hub Not Support : "..data["user-agent"])
+end
+
+
 local pd = game.placeId
 
 if pd == 9621271261 then
